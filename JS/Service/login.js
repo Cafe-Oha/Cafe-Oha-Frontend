588976a1b935439ebd1429ffd14f192f215df52a
mada0245 <80597666+mada0245@users.noreply.github.com>
2022-11-22 15:13:36 +0100

    Login Front-End

---
+++ JS/Service/login.js
@@ -0,0 +1,13 @@
+
+
+function openPopup(){
+    // popup.classList.add('open-popup')
+    popup.style.visibility = "visible"
+    popup.style.transform = "translate(0%,0%) scale(1)";
+}
+function closePopup(){
+    // popup.classList.remove('open-popup')
+    popup.style.visibility = "hidden"
+    popup.style.transform = "translate(0%,0%) scale(0.1)";
+
+}
