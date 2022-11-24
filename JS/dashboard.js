7e8107de133bab35faf8743e10dc271f379f0b01
Emie0038 <Emie0038>
2022-11-24 08:05:51 +0100

    html css sidebar

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,14 +1,5 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+import { getAll  } from "./Service/API_calls.js";
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
+$(function(){
+    $("inventoryPage").load("inventory.html");
+});
