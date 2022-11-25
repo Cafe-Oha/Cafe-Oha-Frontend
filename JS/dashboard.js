b35e268f7c4013fced3d8fcb57c3d282c3d01ef2
Emie0038 <Emie0038>
2022-11-25 08:32:09 +0100

    dashboard js

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,7 +1,13 @@
 import { getAll  } from "./Service/API_calls.js";
 
-$(document).ready(() => {
-    $('.inventoryPage').click(function() {
-
-    })
-})
+window.onload = () => {
+    // copy below to add more pages
+    fetch('C:\\Users\\GloriaB\\IdeaProjects\\Cafe-Oha-Frontend\\HTML\\inventory.html') // the page we want to use for our sidebar
+        .then(data => {
+            return data.text()
+        })
+        .then( data => {
+            document.getElementsByClassName("header").innerHTML = data; // inserts to element class="sidebar"
+        })
+    // copy end
+}
