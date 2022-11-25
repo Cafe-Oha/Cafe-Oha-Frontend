b48c770a45fc6710feae869b93aee01a428ddfe0
Emie0038 <Emie0038>
2022-11-25 14:17:17 +0100

    dashboard almost done

--- JS/dashboard.js
+++ JS/dashboard.js
@@ -1,13 +1,8 @@
-import { getAll  } from "./Service/API_calls.js";
 
-window.onload = () => {
-    // copy below to add more pages
-    fetch('C:\\Users\\GloriaB\\IdeaProjects\\Cafe-Oha-Frontend\\HTML\\inventory.html') // the page we want to use for our sidebar
-        .then(data => {
-            return data.text()
-        })
-        .then( data => {
-            document.getElementsByClassName("header").innerHTML = data; // inserts to element class="sidebar"
-        })
-    // copy end
-}
+
+const nav = document.querySelector('.navbar')
+fetch('navbar.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+})
