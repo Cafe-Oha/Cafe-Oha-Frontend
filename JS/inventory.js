0c063bdec2bbea4fef1a0897343941adad9c81a2
chrispykankea <chri38gh@stud.kea.dk>
2022-11-28 13:42:15 +0100

    the old one working

--- JS/inventory.js
+++ JS/inventory.js
@@ -1,19 +1,51 @@
 
-function addToInventory(){
-    const tbl =  document.getElementById("inventoryTable");
-    let row = tbl.insertRow();
-    const cell1 = row.insertCell();
-    const cell2 = row.insertCell();
-    const cell3 = row.insertCell();
 
-    //add user input into table
-    cell1.innerHTML= document.getElementById("iid").value;
-    cell2.innerHTML= document.getElementById("iquantity").value;
-    cell3.innerHTML= document.getElementById("iunit").value;
+// Add test products to <table>
+function testInventoryAdd() {
+    // First check if a <tbody> tag exists, add one if not
+    if ($("#inventoryTable tbody").length == 0) {
+        $("#inventoryTable").append("<tbody></tbody>");
+    }
 
+    // Append product to the table
+    $("#inventoryTable tbody").append("<tr>" +
+        "<td>Flour</td>" +
+        "<td>20</td>" +
+        "<td>KG</td>" +
+        "</tr>");
+
+    $("#inventoryTable tbody").append("<tr>" +
+        "<td>Oil</td>" +
+        "<td>3</td>" +
+        "<td>L</td>" +
+        "</tr>");
+
+    $("#inventoryTable tbody").append("<tr>" +
+        "<td>Avocado</td>" +
+        "<td>5</td>" +
+        "<td>KG</td>" +
+        "</tr>");
 }
 
+$(document).ready(function () {
+    testInventoryAdd();
+});
 
+
+////////////Ignore the stuff above this line
+
+
+
+function addToInventory() {
+    let a = $("#iid").val();
+    let b = $("#iquantity").val();
+    let c = $("#iunit").val();
+
+    $("#inventoryTable tbody").append("<tr><td>" + a + "</td><td>" + b + "</td><td>" + c + "</td></tr>");
+    $("#iid").val('');
+    $("#iquantity").val('');
+    $("#iunit").val('');
+}
 
 
 function openForm() {
