b918a5e011bf0b8f7fd1f442ff469334def8ef5a
chrispykankea <chri38gh@stud.kea.dk>
2022-11-28 10:17:12 +0100

    messed up. do not use

--- JS/inventory.js
+++ JS/inventory.js
@@ -1,39 +1,3 @@
-//   // Add test products to <table>
-//     function testInventoryAdd() {
-//     // First check if a <tbody> tag exists, add one if not
-//     if ($("#inventoryTable tbody").length == 0) {
-//     $("#inventoryTable").append("<tbody></tbody>");
-// }
-//
-//     // Append product to the table
-//     $("#inventoryTable tbody").append("<tr>" +
-//     "<td>Flour</td>" +
-//     "<td>20</td>" +
-//     "<td>KG</td>" +
-//     "</tr>");
-//
-//     $("#inventoryTable tbody").append("<tr>" +
-//         "<td>Oil</td>" +
-//         "<td>3</td>" +
-//         "<td>L</td>" +
-//         "</tr>");
-//
-//     $("#inventoryTable tbody").append("<tr>" +
-//         "<td>Avocado</td>" +
-//         "<td>5</td>" +
-//         "<td>KG</td>" +
-//         "</tr>");
-// }
-//
-// $(document).ready(function () {
-//     testInventoryAdd();
-// });
-//
-//
-// ////////////Ignore the stuff above this line
-
-
-
 
 function addToInventory(){
     const tbl =  document.getElementById("inventoryTable");
@@ -52,33 +16,10 @@ function addToInventory(){
 
 
 
-  function openForm() {
-      document.getElementById("addInvForm").style.display = "block";
-  }
+function openForm() {
+    document.getElementById("addInvForm").style.display = "block";
+}
 
 function closeForm() {
     document.getElementById("addInvForm").style.display = "none";
 }
-
-
-
-//display all ingredients in table
-
-fetch('http://localhost:8080/ingredients')
-    .then(res => {
-        return res.json();
-    }).then(data => {
-        data.forEach(ingredient => {
-            const markup = `<td>${ingredient.name}</td>
-                            <td>${ingredient.quantity}</td>
-                            <td>${ingredient.unit}</td>`;
-            document.querySelector('table').insertAdjacentHTML('beforeend', markup);
-        });
-    })
-    .catch(err => console.error(err));
-
-
-
-
-
-
