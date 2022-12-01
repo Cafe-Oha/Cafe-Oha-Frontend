47d5a8c811d79c056bb7ab07f121f526e30b738b
mada0245 <80597666+mada0245@users.noreply.github.com>
2022-12-01 14:58:43 +0100

    search items and input filter

--- JS/insertSales.js
+++ JS/insertSales.js
@@ -1,5 +1,5 @@
 ////.......................................................////.....................................................////
-let menuItemName= ["chips","sandwich","soup","toast","salad","applePie"];  //From DB//
+let menuItemName= ["chips","sandwich","soup","toast","salad","applePie"];
 ////.......................................................////.....................................................////
 let namingLable = "Label";
 
@@ -9,6 +9,10 @@ container.innerHTML = "";
 
 //creat an input field in html for each member of the menu item list
 for (let i = 0; i < menuItemName.length; i++) {
+
+    let newLiTag =document.createElement('li')
+    let newATag =document.createElement('a');
+    newATag.href ="#";
 
     let newLable= document.createElement('label');
     newLable.htmlFor = menuItemName[i];
@@ -35,10 +39,11 @@ for (let i = 0; i < menuItemName.length; i++) {
     newSpan.style.marginBottom = "20px";
 
 
-
-    container.appendChild(newLable);
-    container.appendChild(newInput);
-    container.appendChild(newSpan);
+    container.appendChild(newLiTag);
+    newLiTag.appendChild(newATag);
+    newATag.appendChild(newLable);
+    newATag.appendChild(newInput);
+    newATag.appendChild(newSpan);
 
 }
 
@@ -78,8 +83,6 @@ function saveInput() {
 
                 redText.style.color = "red";
                 redText.style.fontWeight = "bold";
-                console.log("this is working");
-
 
             }
     }
@@ -88,8 +91,22 @@ function saveInput() {
 ////.......................................................////.....................................................////
 ////.......................................................////.....................................................////
                                                         //To DB//
+    for (let i = 0; i < arrayInput.length; i++) {
 
-    alert(arrayInput.toString());
+////.......................................................////.....................................................////
+        let itemSaleDate;                               //From DB//
+        let menuItemTotalSaleOfThisDay = 10000;
+////.......................................................////.....................................................////
+
+        let date  = new Date();
+        if(date == itemSaleDate){
+            let newMenuItemTotalSaleOfThisDay = arrayInput[i] + menuItemTotalSaleOfThisDay;
+        }
+
+        let itemSaleInput = arrayInput[i];
+
+    }
+    console.log(arrayInput.toString());
     // connect to the total sale of an menu item and the date
     // sum the value as total sale of the item per date
     // identify the date and connect it to the tables
@@ -98,8 +115,36 @@ function saveInput() {
 ////.......................................................////.....................................................////
 }
 
+
+//Search menu items
+function searchLabels(){
+    // Declare variables
+    let searchiInput, filter, ul, li, a, i, txtValue;
+    searchiInput = document.getElementById('searchBarID');
+    filter = searchiInput.value.toUpperCase();
+
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
+
+
+
 //add a # before each array to represent an id style to use the function utilities.cleanInputFields()
 menuItemName = menuItemName.map(i => '#' + i);
+console.log(menuItemName.toString());
 let inputIdSelector = menuItemName.toString();
 
 //utilities.cleanInputFields()
+
