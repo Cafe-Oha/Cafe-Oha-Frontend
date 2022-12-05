865e568243f5c9be09da6b0db29e42f2693d8f60
Wanesia <wintmiller161@gmail.com>
2022-12-05 15:55:50 +0100

    display ingredients list

--- JS/instructions.js
+++ JS/instructions.js
@@ -26,3 +26,22 @@ async function getIt(){
     menuInstructions.textContent = data.instruction;
 }
 getIt();
+
+//display ingredients list
+const description = document.querySelector('#descriptionTitle')
+fetch('http://localhost:8080/menu')
+    .then(res => {
+        return res.json();
+    }).then(data => {
+
+    console.log('displaying list of ingredients')
+
+    for (let i = 0; i < data.length; i++) {
+        const ing = data[i].menuIngredients;
+        for (let j=0; ing.length; j++){
+            console.log(ing[j].name);
+            const markup = `<ol> <li>${ing[j].name}</li> </ol>`;
+            description.insertAdjacentHTML('beforeend', markup);
+        }
+    }
+});
